118cfbea189a1dbb814229cd45a9eaa012b9b7cd
kristen-killingsworth <killingsworth_kristen@yahoo.com>
2023-07-29 22:56:13 -0400

    added 3rd meow

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ let animal = document.getElementById(`animalH2`);
 let generateBtn = document.getElementById(`generateBtn`);
 let meow1 = document.getElementById(`meow-1`);
 let meow2 = document.getElementById(`meow-2`);
+let meow3 = document.getElementById(`meow-3`);
 let meowCount = 0;
 
 let animalArr = ["Dog","Cow","Cat","Horse","Donkey","Tiger","Lion","Panther","Leopard","Cheetah","Bear","Elephant","Turtle","Tortoise","Crocodile","Rabbit","Porcupine","Hare","Hen","Pigeon","Albatross","Crow","Fish","Dolphin","Frog","Whale","Alligator","Eagle","Flying squirrel","Ostrich","Fox","Goat","Jackal","Emu","Armadillo","Eel","Goose","Wolf","Gorilla","Chimpanzee","Monkey","Beaver","Orangutan","Antelope","Bat","Badger","Giraffe","Hermit Crab","Panda","Hamster","Cobra","Hammerhead shark","Camel","Hawk","Deer","Chameleon","Hippopotamus","Jaguar","Lizard","Koala","Kangaroo","Iguana","Llama","Chinchillas","Jellyfish","Rhinoceros","Hedgehog","Zebra","Possum","Wombat","Bison","Bull","Sheep","Meerkat","Mouse","Otter","Sloth","Owl","Vulture","Flamingo","Racoon","Mole","Duck","Swan","Lynx","Elk","Boar","Lemur","Mule","Baboon","Mammoth","Rat","Snake","Peacock"];
@@ -12,8 +13,11 @@ generateBtn.addEventListener(`click`, () => {
     if (meowCount === 0) {
         meow1.play();
         meowCount++;
-    } else {
+    } else if (meowCount === 1) {
         meow2.play();
+        meowCount++;
+    } else {
+        meow3.play();
         meowCount = 0;
     }
     // select random adjective
